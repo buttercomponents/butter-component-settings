9274036009bf41a09a76184b43e9f2769332093a
btzr-io <btzr.io@gmail.com>
2017-02-22 15:08:00 -0700

    minor fix css

--- src/components/tabs/index.js
+++ src/components/tabs/index.js
@@ -3,7 +3,7 @@ import { translate } from 'react-i18next';
 import style from './theme.styl';
 
 export default (props) => (
-    <ul id="myTabs" className={style['nav', 'nav-tabs']} role="tablist">
+    <ul id="myTabs" className={style.tabs} role="tablist">
         {props.tabs.map((t, i) => (
             <li className={i===0?'active':''} key={i} href={t.id} aria-controls={t.id} role="tab" data-toggle="tab">
                 {props.t(t.title)}
