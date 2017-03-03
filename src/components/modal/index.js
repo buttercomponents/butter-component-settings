beb5b16fd6d2b6bef19429bb71118a75ac649fb2
btzr-io <btzr.io@gmail.com>
2017-03-03 15:31:24 -0700

    implement modal

---
+++ src/components/modal/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { translate } from 'react-i18next';
+import style from './style.styl';
+
+export default class Modal extends Component {
+    constructor(props) {
+        super();
+        this.toggleModal = props.handler.toggleModal || function(){};
+    }
+
+    render (){
+        let {props} = this;
+        return (
+            <div className={style.modal} data-position={props.position} data-show={props.visible}>
+                <div className={style.widget}></div>
+                <div className={style.overlay} onClick={this.toggleModal.bind(this)}></div>
+            </div>
+        )
+    }
+}
