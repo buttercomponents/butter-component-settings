0c30bd64e5ba5349c1ad49920e23f1c1fe137cc5
btzr-io <btzr.io@gmail.com>
2017-03-10 21:50:12 -0700

    implement ReactCSSTransitionGroup

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
-//import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import { translate } from 'react-i18next';
 import style from './style.styl';
 
+
 export default ({position, action}) => (
-    <div className={style.modal} data-position={position} onClick={action.apply}>
-        <div className={style.widget}>
-            <img src={"https://media.giphy.com/media/o0vwzuFwCGAFO/giphy.gif"} />
+    <ReactCSSTransitionGroup transitionName="modal" transitionEnterTimeout={500} transitionLeaveTimeout={300}>
+        <div className={style.modal} data-position={position} onClick={action.apply}>
+            <div className={style.widget}>
+                <img src={"https://media.giphy.com/media/o0vwzuFwCGAFO/giphy.gif"} />
+            </div>
+            <div className={style.overlay}></div>
         </div>
-        <div className={style.overlay}></div>
-    </div>
+    </ReactCSSTransitionGroup>
 )
