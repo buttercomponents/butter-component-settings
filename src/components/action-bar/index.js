c61819281e14a77bd5a6404c4ada344ed3c6feba
Niv Sardi <xaiki@evilgiggle.com>
2017-04-27 02:24:39 -0300

    actionbar: rework to take various actions
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/components/action-bar/index.js
+++ src/components/action-bar/index.js
@@ -3,19 +3,19 @@ import { translate } from 'react-i18next';
 import style from './style.styl';
 import GoBackButton from '../go-back-button';
 
-export default (props) => (
+export default ({t, action, ...props}) => (
     <div id="action-bar" className={style['action-bar']}>
-        <GoBackButton {...props} title="Settings"/>
+        <GoBackButton t={t} action={action.goBack} title="Settings"/>
         <div className={style['buttons']}>
             <li>
-                <i data-toggle="tooltip" data-placement="left" title={props.t('Keyboard Shortcuts')} className="material-icons">keyboard</i>
+                <i data-toggle="tooltip" data-placement="left" title={t('Keyboard Shortcuts')} className="material-icons">keyboard</i>
             </li>
             <li>
-                <i data-toogle="tooltip" data-placement="left" title={props.t('Help Section')} className="material-icons">help_outline</i>
+                <i data-toogle="tooltip" data-placement="left" title={t('Help Section')} className="material-icons">help_outline</i>
             </li>
             <li>
-                <i data-toggle="tooltip" data-placement="left" title={props.t('Show Advanced Settings')} className="material-icons">filter_list</i>
-                <input type="checkbox" onChange={props.action.toggleAdvanced}/>
+                <i data-toggle="tooltip" data-placement="left" title={t('Show Advanced Settings')} className="material-icons">filter_list</i>
+                <input type="checkbox" onChange={action.toggleAdvanced}/>
             </li>
         </div>
     </div>
