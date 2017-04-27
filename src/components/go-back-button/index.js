60a83da71be341b6d14060f9e86e161bc9d11a6e
Niv Sardi <xaiki@evilgiggle.com>
2017-04-27 02:24:55 -0300

    go-back-button: take action and be an <a/>
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/components/go-back-button/index.js
+++ src/components/go-back-button/index.js
@@ -3,8 +3,8 @@ import { translate } from 'react-i18next';
 import style from './style.styl';
 
 export default (props) => (
-    <div className={style['go-back']}>
-       <i className="material-icons">arrow_back</i>
-       <span>{props.t(props.title)}</span>
-    </div>
+    <a className={style['go-back']} onClick={props.action}>
+        <i className="material-icons">arrow_back</i>
+        <span>{props.t(props.title)}</span>
+    </a>
 )
