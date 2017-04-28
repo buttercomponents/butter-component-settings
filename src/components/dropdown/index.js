00ccd8b4016076158df6c5cf777385cacad4728e
Niv Sardi <xaiki@evilgiggle.com>
2017-04-28 13:55:13 -0300

    BUGGY: implement color picker
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/components/dropdown/index.js
+++ src/components/dropdown/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { ChromePicker } from 'react-color';
 import { translate } from 'react-i18next';
 import style from './style.styl';
 
@@ -52,7 +53,9 @@ class Dropdown extends Component {
         let {props, state} = this
         const Item = props.config.item
         const Label = props.config.label
-        const selected = props.options[state.selected]
+        const ExtraComponent = props.extra
+
+        const selected = props.options[state.selected] || state.selected
 
         return  (
             <div className={"boostrap-dropdown " + style[ "dropdown-" + props.config.type] }>
@@ -71,7 +74,8 @@ class Dropdown extends Component {
                             ))
                         }
                     </ul>
-                    {props.children}
+                    {props.extra ? <ExtraComponent selected={selected} apply={this.onSelect.bind(this)}/>:null}
+
                 </div>
             </div>
         )
@@ -88,14 +92,51 @@ Dropdown.defaultProps = {
 
 const colorOpts = {
     type: "color",
+
     item: ColorDropdownItem,
     label: ColorLabelItem
 }
 
-let DropdownColor =  (props) => (
-    <Dropdown config={colorOpts}  {...props}>
-        <DropdownItem value="More colors..."/>
-    </Dropdown>
+class PickerWrapper extends Component {
+    constructor (props) {
+        super()
+        this.state = {
+            show: false
+        }
+    }
+
+    show (e) {
+        this.setState({show: true})
+    }
+
+    changeColor (c) {
+        this.props.apply(c.hex)
+    }
+
+    setColor (c) {
+        //this.setState({show: false})
+        this.props.apply(c.hex)
+    }
+
+
+    render () {
+        let {props, state} = this
+
+        if (state.show) {
+            return (
+                <ChromePicker color={props.selected}
+                              onChange={this.changeColor.bind(this)}
+                              onChangeComplete={this.setColor.bind(this)}
+                />)
+        }
+        return (
+            <DropdownItem value="More colors..." onSelect={this.show.bind(this)}/>
+        )
+    }
+}
+
+let DropdownColor = (props) => (
+    <Dropdown config={colorOpts} extra={PickerWrapper} {...props}/>
 )
 
 export { Dropdown as default, DropdownColor }
